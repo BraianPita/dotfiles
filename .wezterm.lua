811dc8713570c18611514601d47c122fb1ad58e0
Braian Pita <54162188+BraianPita@users.noreply.github.com>
2024-09-08 17:58:19 -0500

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,6 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 12
 
